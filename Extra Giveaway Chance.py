e695657f8721e75f6625ff5691e3ddfab9ca9bd3
Ideklo <77453748+Ideklo@users.noreply.github.com>
2021-01-14 16:26:32 +0000

    Add files via upload

---
+++ Extra Giveaway Chance.py
@@ -0,0 +1,18 @@
+#WHY YOU TRYANA BREAK IN ARE YOU DUM
+print ("Click On That Link To Get Started")
+print ("file:///C:/Users/USER/Desktop/Just%20a%20bunch%20of%20code%20work%23/First%20Link.html")
+
+#
+while True:
+    password = input ("what is the password?")
+    if password == "hkajtian64315ngkan5jqnfkey":
+        print("ACCESS GRANTED")
+        #
+        break
+    else:
+        print("ACCESS DENIED")
+        #
+
+print ("THANK YOU FOR BUYING EGC")
+print ("TO CONTINUE CLICK ON THIS LINK file:///C:/Users/USER/Desktop/Just%20a%20bunch%20of%20code%20work%23/EGC.html")
+print ("once you done that you Just Follow What It Says Goodbye")
